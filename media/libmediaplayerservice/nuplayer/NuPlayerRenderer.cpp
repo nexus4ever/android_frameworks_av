8355e7b795370543817ac72687da08eb96a2e7b6
b50906 <xiahong.bao@nxp.com>
2016-04-29 16:45:26 +0800

    Gallery's progress bar stay at 0 when play some clips
    If audio is eos before video, video should call updateMaxTimeMedia,
    or mediatime will always stay at the position when audio ends.
    
    Change-Id: Ia3104c5a9e8863f4f3021050e711f83d6200f1b3
    Signed-off-by: b50906 <xiahong.bao@nxp.com>
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
@@ -110,6 +110,7 @@ NuPlayer::Renderer::Renderer(
       mVideoLateByUs(0ll),
       mHasAudio(false),
       mHasVideo(false),
+      mAudioEOS(false),
       mNotifyCompleteAudio(false),
       mNotifyCompleteVideo(false),
       mSyncQueues(false),
@@ -1183,7 +1184,7 @@ void NuPlayer::Renderer::postDrainVideoQueue() {
                 realTimeUs = nowUs;
             }
         }
-        if (!mHasAudio) {
+        if (!mHasAudio || mAudioEOS) {
             // smooth out videos >= 10fps
             mMediaClock->updateMaxTimeMedia(mediaTimeUs + 100000);
         }
@@ -1357,6 +1358,7 @@ void NuPlayer::Renderer::onQueueBuffer(const sp<AMessage> &msg) {
 
     if (audio) {
         mHasAudio = true;
+        mAudioEOS = false;
     } else {
         mHasVideo = true;
     }
@@ -1467,6 +1469,7 @@ void NuPlayer::Renderer::onQueueEOS(const sp<AMessage> &msg) {
         }
         mAudioQueue.push_back(entry);
         postDrainAudioQueue_l();
+        mAudioEOS = true;
     } else {
         if (mVideoQueue.empty() && getSyncQueues()) {
             Mutex::Autolock autoLock(mLock);
